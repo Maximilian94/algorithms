dde58a3ff031a0abd7c851ab9e5f6c6e5e23f842
Maximilian Kaden <mkadenfilgo@gmail.com>
2025-03-09 13:24:04 +0100

    QuickSort: Remove redundant temporary arrays in QuickSort
    Eliminates tempGreaterThanList and tempLessThanList, reducing code complexity
    and improving readability. No functional changes made.

--- Quick Sort/quick-sort.typescript.ts
+++ Quick Sort/quick-sort.typescript.ts
@@ -2,39 +2,32 @@ import {generateRandomArray} from "../utils/generateRandomArray";
 
 const randomArray = generateRandomArray(6, 30);
 
-function QuickSort (list:Array<number>) {
-    if(list.length <= 1) return list;
+function QuickSort(list: Array<number>) {
+    if (list.length <= 1) return list;
 
     const lastItemIndex = list.length - 1;
     const pivot = list[lastItemIndex];
 
-    const tempGreaterThanList:Array<number> = []
-    const tempLessThanList:Array<number> = []
+    let greaterThanList: Array<number> = []
+    let lessThanList: Array<number> = []
 
-    let greaterThanList:Array<number> = []
-    let lessThanList:Array<number> = []
-
-    for(let i = 0; i < lastItemIndex; i++) {
+    for (let i = 0; i < lastItemIndex; i++) {
         const item = list[i];
-        if(item >= pivot) tempGreaterThanList.push(item)
-        if(item < pivot) tempLessThanList.push(item)
+        if (item >= pivot) greaterThanList.push(item)
+        if (item < pivot) lessThanList.push(item)
 
     }
 
-    if(tempGreaterThanList.length > 1){
-        greaterThanList = QuickSort(tempGreaterThanList)
-    } else {
-        greaterThanList = tempGreaterThanList
+    if (greaterThanList.length > 1) {
+        greaterThanList = QuickSort(greaterThanList)
     }
 
-    if(tempLessThanList.length > 1){
-        lessThanList = QuickSort(tempLessThanList)
-    } else {
-        lessThanList = tempLessThanList
+    if (lessThanList.length > 1) {
+        lessThanList = QuickSort(lessThanList)
     }
 
     return [...lessThanList, pivot, ...greaterThanList]
 }
 
-console.log('not sorted',randomArray)
+console.log('not sorted', randomArray)
 console.log('sorted', QuickSort(randomArray))
